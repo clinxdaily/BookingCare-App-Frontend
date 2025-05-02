ac06ab543c34632d97d28170dffac8cb5c2f0e3b
Clinx Daily <clinxdaily@gmail.com>
2025-05-02 11:59:53 +0700

    #40

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,14 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import {getAllUsers} from '../../services/userService';
+import ModalUser from './ModalUser';
 class UserManage extends Component {
 
     constructor(props){
         super(props);
         this.state={
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -21,14 +23,31 @@ class UserManage extends Component {
         }
     }
 
-
+    handleAddNewUser=()=>{
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+    toggleUserModal=()=>{
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
     render() {
         console.log('check state: ', this.state);
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test ={'abc' }
+                />
                 <div className='title text-center'>
                     Manage users
+                </div>
+                <div className='mx-1 '>
+                    <button className='btn btn-primary px-3' onClick={()=>this.handleAddNewUser()}><i className="fas fa-plus"></i> Add new users</button>
                 </div>
                 <div className='users-table mt-3 mx-1'>
                 <table id="customers">
