cd2d9d527ca9ea002f1ece6b38a569a426886885
Clinx Daily <clinxdaily@gmail.com>
2025-05-04 23:24:13 +0700

    #42

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -3,6 +3,7 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
+import {emitter} from "../../utils/emitter";
 class ModalUser extends Component {
   constructor(props) {
     super(props);
@@ -13,9 +14,23 @@ class ModalUser extends Component {
       lastName: "",
       address: "",
     };
-  }
 
-  componentDidMount() {}
+    this.listenToEmitter();
+  }
+  listenToEmitter() {
+    emitter.on("EVENT_CLEAR_MODAL_DATA", () => {
+      this.setState({
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+      });
+    });
+  }
+  componentDidMount() {
+    console.log("mouting model", this.props);
+  }
 
   toggle = () => {
     this.props.toggleFromParent();
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
-import { getAllUsers, createNewUserService } from "../../services/userService";
+import { getAllUsers, createNewUserService, deleteUserService} from "../../services/userService";
 import ModalUser from "./ModalUser";
 import { reject } from "lodash";
+import { emitter } from "../../utils/emitter";
+
 class UserManage extends Component {
   constructor(props) {
     super(props);
@@ -46,11 +48,28 @@ class UserManage extends Component {
             this.setState({
                 isOpenModalUser: false
             })
+            emitter.emit("EVENT_CLEAR_MODAL_DATA"); //clear data đã nhập trước đó 
         }
     } catch (error) {
         reject(error);
     }
    
+  }
+  handleDeleteUser = async(user) => {
+    console.log("delete", user);
+    try {
+      let respone = await deleteUserService(user.id);
+      if (respone && respone.errCode === 0) {
+        await this.getAllUsers();
+      } else {
+        alert(respone.errMessage);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  }
+  handleEditUser= () => {
+    alert("edit");
   }
   render() {
    
@@ -92,10 +111,10 @@ class UserManage extends Component {
                       <td> {item.address}</td>
                       <td>
                         <button className="btn-edit">
-                          <i className="fas fa-pencil-alt"></i>
+                          <i className="fas fa-pencil-alt" onClick={()=>{this.handleEditUser()}}></i>
                         </button>
                         <button className="btn-delete">
-                          <i className="fas fa-trash"></i>
+                          <i className="fas fa-trash" onClick={()=>{this.handleDeleteUser(item)}}></i>
                         </button>
                       </td>
                     </tr>
